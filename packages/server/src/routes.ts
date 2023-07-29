0328b5cbc3db2a5754d2a8913d64d79dfe00c09a
Rahien <karel.kremer@gmail.com>
2023-07-29 11:39:02 +0200

    split arbitrary files

--- packages/server/src/routes.ts
+++ packages/server/src/routes.ts
@@ -1,11 +1,22 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
 import { transcribe } from "./transcriber";
+
+import multer from "multer";
+
+const upload = multer({ dest: process.env.WORK_DIR || "/tmp" });
 
 const routes = Router();
 
-routes.get("/transcribe", async (req, res) => {
-  const result = await transcribe();
-  return res.json({ message: result });
-});
+routes.post(
+  "/transcribe",
+  upload.single("file"),
+  async (req: Request, res: Response) => {
+    console.log(req.body);
+    console.log(req.file);
+
+    const id = await transcribe(req.file);
+    res.send({ id });
+  }
+);
 
 export default routes;
--- packages/server/src/transcriber.ts
+++ packages/server/src/transcriber.ts
@@ -1,6 +1,13 @@
 import { Stream } from "stream";
+import { v4 } from "uuid";
 
 const { exec } = require("child_process");
+
+type StatusRecord = {
+  originalname: string;
+  started: number;
+};
+const status = {} as Record<string, StatusRecord>;
 
 function streamToString(stream: Stream) {
   const chunks = [] as Buffer[];
@@ -22,19 +29,45 @@ const runShellCommand = async (command: string) => {
 
 const splitMp3 = async (target: string) => {
   const WORK_DIR = process.env.WORK_DIR || "/tmp";
-  const command = `ffmpeg -loglevel repeat+level+error -i ${WORK_DIR}/${target}/input.mp3 -f segment -segment_time 120 -c copy ${WORK_DIR}/${target}/out%03d.mp3`;
+  const command = `ffmpeg -y -loglevel repeat+level+error -i ${WORK_DIR}/${target}/input.mp3 -f segment -segment_time 120 -c copy ${WORK_DIR}/${target}/out%03d.mp3`;
   console.log(command);
-  const result = await runShellCommand(command);
-  return result;
+  await runShellCommand(command);
+  const removeCommand = `rm ${WORK_DIR}/${target}/input.mp3`;
+  await runShellCommand(removeCommand);
 };
 
-const transformToMp3 = async (target: string, original: string) => {
+const transformToMp3 = async (target: string, mimetype: string) => {
   const WORK_DIR = process.env.WORK_DIR || "/tmp";
-  const command = `ffmpeg -loglevel repeat+level+error -i ${WORK_DIR}/${target}/${original} -vn -ar 44100 -ac 2 -ab 192k -f mp3 ${WORK_DIR}/${target}/input.mp3`;
+  if (mimetype === "audio/mp3") {
+    const moveCommand = `mv ${WORK_DIR}/${target}/original ${WORK_DIR}/${target}/input.mp3`;
+    await runShellCommand(moveCommand);
+    return;
+  }
+  const command = `ffmpeg -y -loglevel repeat+level+error -i ${WORK_DIR}/${target}/original -vn -ar 44100 -ac 2 -ab 192k -f mp3 ${WORK_DIR}/${target}/input.mp3`;
+  await runShellCommand(command);
+  const removeCommand = `rm ${WORK_DIR}/${target}/original`;
+  await runShellCommand(removeCommand);
+};
+
+const moveToWorkDir = async (path: string, target: string) => {
+  const WORK_DIR = process.env.WORK_DIR || "/tmp";
+  const command = `mkdir ${WORK_DIR}/${target} && mv ${path} ${WORK_DIR}/${target}/original`;
   await runShellCommand(command);
 };
 
-export const transcribe = async () => {
-  await transformToMp3("test", "original.m4a");
-  return splitMp3("test");
+export const transcribe = async (file: {
+  path: string;
+  mimetype: string;
+  originalname: string;
+}) => {
+  const id = v4();
+  status[id] = {
+    originalname: file.originalname,
+    started: new Date().getTime(),
+  };
+  moveToWorkDir(file.path, id).then(async () => {
+    await transformToMp3(id, file.mimetype);
+    await splitMp3(id);
+  });
+  return id;
 };
